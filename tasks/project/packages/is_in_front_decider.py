cde15e2fb96c98a37a4b9d86b116adcc2e83a8e8
Iklikito <iklikito2@gmail.com>
2026-06-01 14:06:43 +0400

    Added test images

--- tasks/project/packages/is_in_front_decider.py
+++ tasks/project/packages/is_in_front_decider.py
@@ -2,4 +2,4 @@ from ObjectDetector import ObjectDetector
 import cv2
 
 def is_in_front(frame, object_detector) -> bool:
-    return True
+    raise NotImplementedError("is_in_front is not implemented yet")
