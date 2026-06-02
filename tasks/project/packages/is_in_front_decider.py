f27ed5e468aa2727480234854523f80813c6c6c4
Iklikito <iklikito2@gmail.com>
2026-06-02 16:11:37 +0400

    Implemented is_in_front_decider

--- tasks/project/packages/is_in_front_decider.py
+++ tasks/project/packages/is_in_front_decider.py
@@ -1,2 +1,10 @@
-def is_in_front(frame, detected_objects) -> bool:
-    raise NotImplementedError("is_in_front is not implemented yet")
+import cv2
+import numpy as np
+
+def is_in_front(frame) -> bool:
+    h   = frame.shape[0]
+    roi = frame[int(h * 0.75):, :]
+    hsv = cv2.cvtColor(roi, cv2.COLOR_BGR2HSV)
+    mask1 = cv2.inRange(hsv, np.array([0,   120, 100]), np.array([10,  255, 255]))
+    mask2 = cv2.inRange(hsv, np.array([170, 120, 100]), np.array([180, 255, 255]))
+    return int(np.count_nonzero(mask1 | mask2)) > 600
